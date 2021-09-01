ca5e70ec089f371dec493c424692aead54e15b4b
Alex TU <tuchaoxu1219@sina.com>
2021-09-01 18:12:14 +0800

    优化

--- gulpfile.js
+++ gulpfile.js
@@ -18,6 +18,14 @@ var paths = {
 }
 
 gulp.task('default', async () => {
+  gulp.src('src/markdown.js')//{sourcemaps:true}   src/*.js
+    .pipe(babel())
+    .pipe(concat('atu.markdown.0.1.0.js'))
+    .pipe(gulp.dest('build'))
+    .on('error', function (err) {
+      gutil.log(gutil.colors.red('[Error]'), err.toString());
+   });
+
   gulp.src(paths.scripts)//{sourcemaps:true}   src/*.js
     .pipe(babel())
     .pipe(concat('atu.' + version + '.js'))
@@ -25,7 +33,7 @@ gulp.task('default', async () => {
     .on('error', function (err) {
       gutil.log(gutil.colors.red('[Error]'), err.toString());
     });
-
+   
   gulp.src(paths.scripts)
     .pipe(babel())
     .pipe(uglify())//压缩  
@@ -36,7 +44,10 @@ gulp.task('default', async () => {
     });
 
   //  复制文件到这里
-  copyFile('./build/atu.1.1.8.js', '../../alextu.com/static/js/', "atu.1.1.8.js");
+  var needCoppyFiles=['atu.1.1.8.js','atu.1.1.8.min.js','atu.markdown.0.1.0.js']
+  for(let i=0;i<needCoppyFiles.length;i++){
+    copyFile('./build/'+needCoppyFiles[i], '../../alextu.com/static/js/', needCoppyFiles[i]);
+  }
 });
 
 function copyFile(orgfilepath, desdirpath, desfilename) {
@@ -45,7 +56,7 @@ function copyFile(orgfilepath, desdirpath, desfilename) {
   
     if (fs.existsSync(desfilepath)) {
       fs.unlinkSync(desfilepath)
-      console.error("["+Date().toString()+"] alextu.com/static/js/atu.1.1.8.js 更新");
+      console.error("["+Date().toString()+"] alextu.com/static/js/"+desfilename+" 更新");
     }
     fs.copyFileSync(orgfilepath, desfilepath);
   } else {
